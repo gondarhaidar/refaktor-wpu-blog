318885cfef803a0cf2dc6f54ce8280a80e1d5e91
gondarhaidar <gondarhaidar@gmail.com>
2025-04-04 14:47:21 +0700

    mengubah model category

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Category extends Model
 {
     use HasFactory;
+    protected $guarded = ['id'];
     public function posts(){
         return $this->hasMany(Post::class);
     }
